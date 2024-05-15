c3fa593cb3a486e09533f867011bbe0d82672f72
FaridYoucef <songortekinn@gmail.com>
2024-05-15 18:56:09 +0100

    Setting Api slice

---
+++ frontend/src/slices/userApiSlice.js
@@ -0,0 +1,17 @@
+import { apiSlice } from "./apiSlice";
+
+const USERS_URL = "/api/users";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/AUTH`,
+        method: "POST",
+        body: data
+      }),
+    }),
+  }),
+});
+
+export const { userLoginMutation } = usersApiSlice;
