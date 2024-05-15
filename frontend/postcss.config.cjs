32d0ededb9204bb5dddd97cad0d7c1f530b51d63
FaridYoucef <songortekinn@gmail.com>
2024-05-15 02:03:16 +0100

    Frontend

---
+++ frontend/postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+      require('tailwindcss'),  // Include Tailwind CSS
+      require('autoprefixer'), // Include Autoprefixer
+      // Add other PostCSS plugins here if needed
+    ]
+  }
+  
