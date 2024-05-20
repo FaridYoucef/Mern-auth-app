6546d449e066db5352658889be8a0f3a78fec538
FaridYoucef <songortekinn@gmail.com>
2024-05-20 04:59:22 +0100

    Profile component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./screens/Register";
 import Login from "./screens/Login";
+import Profile from "./screens/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -15,13 +17,17 @@ const App = () => {
     <BrowserRouter>
       <>
         <Navbar />
-        <ToastContainer/>
+        <ToastContainer />
         <Routes>
-          <Route path="/" element={< Home/>} />
-          <Route path="/signup" element={< Register/>} />
-          <Route path="/signin" element={< Login/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/signin" element={<Login />} />
+
+          {/* Private Routes */}
+          <Route path="" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
-       
       </>
     </BrowserRouter>
   );
---
+++ frontend/src/screens/Profile.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
+
+const Profile = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+
+
+  const dispatch = useDispatch();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  //if there is userInfo it means thats the user is logged in
+  // if logged in we go (navigate) to the login screen using useEffect
+  useEffect(() => {
+    setName(userInfo.name);
+    setEmail(userInfo.email);
+  }, [userInfo.setName, userInfo.setEmail]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("password does not mutch");
+    } else {
+      try {
+      } catch (err) {
+        toast.error(err?.data.message || err.error);
+      }
+    }
+  };
+  return (
+    <div className="flex justify-center items-center h-screen  ">
+    <form
+      onSubmit={submitHandler}
+      className=" max-w-md  p-10 bg-slate-100 rounded-xl  flex flex-col shadow-lg"
+    >
+      <h2 className=" text-2xl font-bold text-center pb-3">Update Profile</h2>
+
+      <label htmlFor="text" className="py-3">
+        User name
+      </label>
+      <input
+        type="name"
+        placeholder="Enter your name"
+        className="p-1 rounded-full py-2"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <label htmlFor="email" className="py-3">
+        Email
+      </label>
+      <input
+        type="email"
+        placeholder="email"
+        className="p-1 rounded-full py-2"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <label htmlFor="password" className="py-3">
+        Passsword
+      </label>
+      <input
+        type="password"
+        placeholder="Enter your name"
+        className="p-1 rounded-full py-2"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <label htmlFor="text" className="py-3">
+        Confirm Password
+      </label>
+      <input
+        type="password"
+        placeholder="Enter your name"
+        className="p-1 rounded-full  py-2"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+
+      <button className="my-4 py-2 text-white bg-[#292929] hover:text-slate-300 rounded-full w-32 mx-auto">
+        Update
+      </button>
+    </form>
+  </div>
+  );
+};
+
+export default Profile;
